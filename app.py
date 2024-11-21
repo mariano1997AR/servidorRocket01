8dfd44e76e4bff600cab90e4ffff8c828fcf0a8d
Mariano <mariano.andres1024@gmail.com>
2024-11-21 17:32:31 -0300

    todos los datos

--- app.py
+++ app.py
@@ -11,36 +11,46 @@ CORS(app) # Esta habilita CORS para todas las rutas
 # Ruta POST para recibir datos desde React y enviar una respuesta
 @app.route('/api/submitdata',methods=['POST'])
 def submit_data():
-     # Leer el dato enviado desde el cliente
-    data = request.json  # Flask espera un JSON
-    mensaje = data.get('accion', '')  # Obtener el valor del campo "accion"
+    try:
+         # Leer el dato enviado desde el cliente
+      data = request.json  # Flask espera un JSON
+      mensaje = data.get('accion', '')  # Obtener el valor del campo "accion"
 
-    if mensaje == 'crear':
-        # Crear el documento PDF
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font('Arial', size=12)
-        pdf.cell(200, 10, txt="Este es tu documento generado.", ln=True, align='C')
+      if mensaje == 'crear':
+         # Crear el documento PDF
+         pdf = FPDF()
+         pdf.add_page()
+         pdf.set_font('Arial', size=12)
+         pdf.cell(200, 10, txt="Este es tu documento generado.", ln=True, align='C')
 
-        # Guardar el PDF temporalmente
-        file_path = 'documento.pdf'
-        pdf.output(file_path)
+         # Guardar el PDF temporalmente
+         file_path = 'documento.pdf'
+         pdf.output(file_path)
 
-        # Enviar el archivo como respuesta
-        response = send_file(file_path, as_attachment=True)
+         # Enviar el archivo como respuesta
+         response = send_file(file_path, as_attachment=True)
 
-        # Agregar eliminación del archivo tras enviar la respuesta
-        @response.call_on_close
-        def remove_file():
-            if os.path.exists(file_path):
-                os.remove(file_path)
+         # Agregar eliminación del archivo tras enviar la respuesta
+         @response.call_on_close
+         def remove_file():
+             if os.path.exists(file_path):
+                 os.remove(file_path)
 
-        return response
-    elif mensaje.lower() == 'saludar'.lower():
-        # Responder con el mensaje recibido 
-        respuestaSaludar = "Hola rocket te saluda"
-        print(respuestaSaludar)
-        return jsonify({"mensaje": f" {respuestaSaludar}"}), 200
+         return response
+      elif mensaje.lower() == 'saludar'.lower():
+         # Responder con el mensaje recibido 
+         respuestaSaludar = "Hola rocket te saluda"
+         print(respuestaSaludar)
+         return jsonify({"mensaje": f" {respuestaSaludar}"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}),500
+
+# Manejar las solicitudes OPTIONS explícitamente
+@app.route('/api/submitdata', methods=['OPTIONS'])
+def options():
+    response = jsonify()
+    response.status_code = 200
+    return response
 
 @app.after_request
 def after_request(response):
